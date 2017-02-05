e9d8efdd3095035ef7885b546898da0dcde9b64b
AJUdensi <a.joshuaudensi@gmail.com>
2017-02-05 19:09:34 +0100

    Factorial and Fibonacci

--- simplenumbers.js
+++ simplenumbers.js
@@ -1,94 +1,136 @@
-var SimpleNumbers = function (config) {
+let SimpleNumbers = (function () {
 
-	(!(config) || (config.constructor !== Object) ? config = {
-		type: 'even',
-		stride: 1,
-		maximumNumber: 0
-	} : config);
+	let SimpleNumbers = function (config) {
 
-	var producedNumbers = [];
+		let producedNumbers = [];
 
-	this.numberGenerator = function (max, start, stride) {
-		producedNumbers = []; //reset variable to empty. Remove all variables
-		max = max || 0;
-		start = start || false;
-		stride = stride || 1;
-		((typeof max === 'number') ? max : (!(config.maximumNumber) ? max : max = config.maximumNumber));
-		((start === true) ? start = 1 : start = 0);
-		((typeof stride === 'number') ? stride : (!(config.stride) ? stride : stride = config.stride));
-		for (let i = start; i <= max; i += stride) {
-			producedNumbers.push(i);
+		(!(config) || (config.constructor !== Object) ? config = {
+			type: 'even',
+			stride: 1,
+			maximumNumber: -1
+		} : config);
+
+		if (!(config.stride)) {
+			config.stride = 1;
 		}
-		return this;
-	};
+		if (!(config.maximumNumber)) {
+			config.maximumNumber = -1;
+		}
 
-	this.getNumbers = function (max, start = false) {
-		producedNumbers = []; //reset variable to empty. Remove all variables
-		this.numberGenerator(max, start);
-		var result = producedNumbers;
-		return result;
-	};
 
-	this.produceNumberType = function (type) {
 
-		((type) ? config.type = type : (config.type && !(type)) ? config.type : 'even');
-
-		if (!config.type && !type) config.type = 'even';
-
-		var numType = ['even', 'odd', 'prime'];
-
-		if (numType.indexOf(config.type) !== -1) {
-			if (config.type === 'odd') {
-				return producedNumbers.filter((a) => {
-					return ((a & 1) ? a : !1);
-				});
+		this.numberGenerator = function (max, start, stride) {
+			producedNumbers = []; //reset variable to empty. Remove all variables
+			max = ((typeof max === 'number') ? max : ((typeof config.maximumNumber === 'number') ? config.maximumNumber : max));
+			stride = ((typeof stride === 'number') ? stride : ((typeof config.stride !== 'number') ? 1 : config.stride));
+			start = ((start === true) ? 1 : 0);
+			for (let i = start; i <= max; i += stride) {
+				producedNumbers.push(i);
 			}
+			return this;
+		};
 
-			if (config.type === 'even') {
-				return producedNumbers.filter((a) => {
-					return (!(a & 1) ? a : !1);
-				});
-			}
+		this.getNumbers = function (max, start, stride) {
+			start = start || false;
+			producedNumbers = []; //reset variable to empty. Remove all variables
+			this.numberGenerator(max, start, stride);
+			var result = producedNumbers;
+			return result;
+		};
 
-			if (config.type === 'prime') {
-				var primes = [];
-				var check = [],
-					i, j;
+		this.produceNumberType = function (type, typeConfig) {
 
-				for (i = 2; i <= producedNumbers[producedNumbers.length - 1]; ++i) {
-					if (!check[i]) {
-						primes.push(i);
-						for (j = i << 1; j <= producedNumbers[producedNumbers.length - 1]; j += i) {
-							check[j] = true;
+			((type) ? config.type = type : (config.type && !(type)) ? config.type : 'even');
+
+			(!(typeConfig) || (typeConfig.constructor !== Object) ? typeConfig = {
+				start: 1,
+				asMax: false
+			} : typeConfig);
+
+			if (!config.type && !type) config.type = 'even';
+
+			var numType = ['even', 'odd', 'prime', 'fact', 'fibo'];
+
+			if (numType.indexOf(config.type) !== -1) {
+				if (config.type === 'odd') {
+					return producedNumbers.filter((a) => {
+						return ((a & 1) ? a : !1);
+					});
+				}
+
+				if (config.type === 'even') {
+					return producedNumbers.filter((a) => {
+						return (!(a & 1) ? a : !1);
+					});
+				}
+
+				if (config.type === 'prime') {
+					var primes = [];
+					var check = [],
+						i, j;
+
+					for (i = 2; i <= producedNumbers[producedNumbers.length - 1]; ++i) {
+						if (!check[i]) {
+							primes.push(i);
+							for (j = i << 1; j <= producedNumbers[producedNumbers.length - 1]; j += i) {
+								check[j] = true;
+							}
 						}
 					}
+					return primes;
 				}
-				return primes;
+
+				if (config.type === 'fact') {
+					let factorial = function (n) {
+						if (n === 0) {
+							return 1;
+						} else {
+							return factorial(n - 1) * n;
+						}
+					};
+					return factorial(producedNumbers[producedNumbers.length - 1]);
+				}
+
+				if (config.type === 'fibo') {
+					let doFibo = function (n) {
+						let fibo = [typeConfig.start];
+						for (let i = typeConfig.start; i < n; i++) {
+							if ((typeConfig.asMax === true) && (fibo[fibo.length - 1] > n)) {
+								fibo.pop();
+								break;
+							}
+							((fibo.length < 2) ? fibo.push(i) : fibo.push(fibo[fibo.length - 1] + fibo[fibo.length - 2]));
+						}
+						return fibo;
+					};
+					return doFibo(producedNumbers[producedNumbers.length - 1]);
+				}
+
+
+			} else {
+				return false;
 			}
+		};
 
-		} else {
-			return false;
-		}
+
+		this.calculate = function () {
+			return Array.prototype.pop.apply(arguments).apply(null, arguments);
+		};
+
+		this.sum = function () {
+			var argumentsToArray = Array.prototype.slice.call(arguments);
+			return argumentsToArray.concat(producedNumbers).reduce((a, b) => {
+				return a + b;
+			});
+		};
+
+		this.diff = function () {
+			var argumentsToArray = Array.prototype.slice.call(arguments);
+			return argumentsToArray.concat(producedNumbers).reduce((a, b) => {
+				return a - b;
+			});
+		};
 	};
-
-
-	this.calculate = function () {
-		return Array.prototype.pop.apply(arguments)
-            .apply(null, arguments);
-	};
-
-	this.sum = function () {
-		var argumentsToArray = Array.prototype.slice.call(arguments);
-		return argumentsToArray.concat(producedNumbers).reduce((a, b) => {
-			return a + b;
-		});
-	};
-
-	this.diff = function () {
-		var argumentsToArray = Array.prototype.slice.call(arguments);
-		return argumentsToArray.concat(producedNumbers).reduce((a, b) => {
-			return a - b;
-		});
-	};
-};
+	return SimpleNumbers;
+})();
 module.exports = SimpleNumbers;
